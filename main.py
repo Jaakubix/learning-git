93ef0a63e01efa44d3105f0dc6e9246c3205fa6c
Jaakubix <93036887+Jaakubix@users.noreply.github.com>
2021-10-23 13:34:49 +0200

    Fix sketch info

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-print("Lista zakupów")
-wartosc = []
-zakupy = {
-    "piekarnia": ["chleb", "bułki", "pączek"],
-    "warzywniak": ["marchew", "seler", "Rukola"]
-}
-for sklep, rzeczy in zakupy.items():
-    sklep = sklep.capitalize()
-    rzeczy[0] = rzeczy[0].capitalize()
-    rzeczy[1] = rzeczy[1].capitalize()
-    rzeczy[2] = rzeczy[2].capitalize()
-    wartosc.append(rzeczy)
-    x = f"Idę do {sklep}, kupuję tu następujące rzeczy {rzeczy}"
-    print(x)
-wartosc1 = len(wartosc[0]) + len(wartosc[1])
-print(f"W sumie kupuję {wartosc1} produktów.")
+print("'Hiszpańska inkwizycja' to najlepszy sketch Monty Pythona")
